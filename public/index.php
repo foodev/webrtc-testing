1a0ec5b729cd08b3aa13e6332eea3b5593e4272a
foodev <jonas@foodev.de>
2016-10-09 13:44:11 +0200

    Use more STUN servers

--- public/index.php
+++ public/index.php
@@ -122,7 +122,13 @@ main #localVideo {
     var peerConnection = new RTCPeerConnection({
         iceServers: [
             {
-                urls: 'stun:stun.l.google.com:19302'
+                urls: [
+                    'stun:stun.l.google.com:19302',
+                    'stun:stun1.l.google.com:19302',
+                    'stun:stun2.l.google.com:19302',
+                    'stun:stun3.l.google.com:19302',
+                    'stun:stun4.l.google.com:19302'
+                ]
             }
         ]
     });
